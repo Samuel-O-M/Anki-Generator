5719404027d2d84d6019a9673bafd9130e108f7b
Samuel Orellana Mateo <samuel.orellanamateo@duke.edu>
2024-08-06 12:46:01 +0200

    Load example for testing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from scrappers.wikitionary import *
 from flashcardCreator import *
 from chatGPTapi import *
 import random
@@ -7,16 +8,17 @@ import re
 
 
 class Word:
-    def __init__(self, top):
+    def __init__(self, top, filename='data/cards.json'):
 
         self._top = str(top)
+        self.filename = filename
 
-        if not os.path.exists('data/cards.json'):
-            with open('data/cards.json', 'w', encoding='utf-8') as file:
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w', encoding='utf-8') as file:
                 json.dump({}, file)
 
         try:
-            with open('data/cards.json', 'r', encoding='utf-8') as file:
+            with open(self.filename, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 if isinstance(self._top, str) and self._top in data:
                     self._tlang = data[self._top].get("tlang", "")
@@ -39,14 +41,10 @@ class Word:
         except json.JSONDecodeError:
             self.clear()
 
-    def initialize(self, driver):
-        word, category = rank(self.top)
-        self._tlang = stress_mark(word, driver)
-
-        if category == "other" and (categorize(word) == "pronoun" or categorize(word) == "error"):
-            self._tag = "error"
-        else:
-            self._tag = category
+    def initialize(self, metadata):
+        self._tlang = metadata["words"][int(self._top)-1]
+        category = categorize(self._tlang, metadata["tlang"])
+        self._tag = category
     
     def clear(self):
         self._tlang = ""
@@ -77,7 +75,7 @@ class Word:
 
     def update(self):
         try:
-            with open('data/cards.json', 'r', encoding='utf-8') as file:
+            with open(self.filename, 'r', encoding='utf-8') as file:
                 data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             data = {}
@@ -98,70 +96,43 @@ class Word:
             "tag": self.tag
         }
 
-        with open('data/cards.json', 'w', encoding='utf-8') as file:
+        with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
 
-def get_prompt(word, tag):
+def get_prompt(word, tlang, langa, langb):
 
-    criteria = ""
-    indication1 = ""
-    indication2 = ""
-    indication3 = ""
+    if langb != "":
 
-    if tag == "noun" or tag == "adjective":
-        criteria = "\nCriteria:\n"
-        criteria += "Ensure using a different declension for each sentence or expression. Always use the singular form."
-        indication1 = "using a declension"
-        indication2 = "using another declension"
-        indication3 = "using a different declension"
-
-    if tag == "pronoun":
-        criteria = "\nCriteria:\n"
-        criteria += "Ensure using different variations for each sentence or expression. which can include gender or case. None of the examples can be nominative."
-        indication1 = "using a variation"
-        indication2 = "using another variation"
-        indication3 = "using a different variation"
-
-    if tag == "verb":
-        choice1 = random.choice(['я', 'ты', random.choice(['он', 'она'])])
-        choice2 = random.choice(['мы', 'вы', 'они'])
-        criteria = "\nCriteria:\n"
-        criteria += f"Ensure using different conjugations for each sentence or expression. Include one example conjugation with {choice1} in present tense, one example conjugation with {choice2}, and one example conjugation in past tense."
-        indication1 = "using a conjugation in present tense with " + choice1
-        indication2 = "using a conjugation in present tense with " + choice2
-        indication3 = "using a conjugation in past tense"
-
-    if tag == "adverb" or tag == "numeral":
-        criteria = "\nCriteria:\n"
-        criteria += "Ensure using the original form for the first example and two different variations for the other examples."
-        indication1 = "using the original form"
-        indication2 = "using another variation"
-        indication3 = "using a different variation"
-
-    prompt = f"""Task: Create one entry of this table with examples and translations for the word {word} to english and spanish. The examples should have a focus on giving a good context and intuition for the word that is being learned. Paste only the final table.
-{criteria}
-
+        prompt = f"""Task: Create one entry of this table with examples and translations for the word {word} in {tlang} to {langa} and {langb}. The examples should have a focus on giving a good context and intuition for the word that is being learned. Paste only the final table.
 Header:
 - Russian: {word} in its original or primitive form
 - English: Translation to English
 - Spanish: Translation to Spanish
-- Russian 1: First sentence or expression {indication1} 
-- Russian 2: Second sentence or expression {indication2}
-- Russian 3: Third sentence or expression {indication3}
+- Russian 1: First sentence or expression
+- Russian 2: Second sentence or expression
+- Russian 3: Third sentence or expression
 - English 1: Translation of Russian 1
 - English 2: Translation of Russian 2
 - English 3: Translation of Russian 3
 - Spanish 1: Translation of Russian 1
 - Spanish 2: Translation of Russian 2
 - Spanish 3: Translation of Russian 3
-    
-Example:
-| Russian   | English  | Spanish   | Russian 1                        | Russian 2                    | Russian 3                      | English 1                    | English 2                    | English 3                      | Spanish 1                      | Spanish 2                    | Spanish 3                      |
-|-----------|----------|-----------|----------------------------------|------------------------------|--------------------------------|------------------------------|------------------------------|--------------------------------|--------------------------------|------------------------------|--------------------------------|
-| человек   | person   | persona    | Я встретил интересного человека. | Он стал хорошим человеком.   | Человеку свойственно ошибаться. | I met an interesting person. | He became a good person.     | It's human to make mistakes.  | Conocí a una persona interesante. | Él se convirtió en una buena persona. | Es humano equivocarse.       |"""
 
-    return prompt
+Example:
+| {tlang}   | {langa}  | {langb}   | {tlang} 1                        | {tlang} 2                    | {tlang} 3                      | {langa} 1                    | {langa} 2                    | {langa} 3                      | {langb} 1                      | {langb} 2                    | {langb} 3                      |
+|-----------|----------|-----------|----------------------------------|------------------------------|--------------------------------|------------------------------|------------------------------|--------------------------------|--------------------------------|------------------------------|--------------------------------|
+"""
+    else:
+
+        prompt = f"""Task: Create one entry of this table with translations for the word {word} in {tlang} to {langa}. Provide examples, which should have a focus on giving a good context and intuition for the word that is being learned. Paste only the final table.
+
+Table: 
+| {tlang}   | {langa}  | {langb}   | Example {tlang} 1                        | Example {tlang} 2                    | Example {tlang} 3                      | Example {langa} 1                    | Example {langa} 2                    | Example {langa} 3                      |                   |
+|-----------|----------|-----------|----------------------------------|------------------------------|--------------------------------|------------------------------|------------------------------|--------------------------------|
+"""
+    return prompt 
+
 
 def normalize_input(input_string):
     elements = re.split(r'[,\s]+', input_string)
@@ -174,51 +145,72 @@ def normalize_input(input_string):
 if __name__ == "__main__":
 
     # INPUT
-    words = normalize_input(input("Enter the words to process: "))
-    proceed = input(f"""
-The following words will be processed: {words}
-Do you want to proceed? (y/n): """)
-    
-    if proceed.lower() != "y":
-        exit()
+    with open('input.txt', 'r', encoding='utf-8') as file:
+        words = file.read()
 
-    name = input("Enter the name of the deck: ")
-    if name == "":
-        name = "deck"
-    
-    tlang = ""
-    while tlang == "":
-        tlang = input("Enter the target language: ")
-        if tlang == "":
-            print("Target language is required.")
-        if tlang.lower() == "exit":
-            exit()
-    tlang = tlang[0].upper() + tlang[1:].lower()
+    normalized_words = normalize_input(words)
 
-    langa = ""
-    while langa == "":
-        langa = input("Enter the first language: ")
-        if langa == "":
-            print("First language is required.")
-        if langa.lower() == "exit":
-            exit()
-    langa = langa[0].upper() + langa[1:].lower()
+#     proceed = input(f"""
+# The following words will be processed: {normalized_words}
+# Do you want to proceed? (y/n): """)
     
-    langb = input("Enter the second language: ")
-    if langb.lower() == "exit":
-        exit()
-    langb = langb[0].upper() + langb[1:].lower()
+    # if proceed.lower() != "y":
+    #     exit()
+
+    # name = input("Enter the name of the deck: ")
+    # if name == "":
+    #     name = "deck"
+    name = "deck"
     
+    # tlang = ""
+    # while tlang == "":
+    #     tlang = input("Enter the target language: ")
+    #     if tlang == "":
+    #         print("Target language is required.")
+    #     if tlang.lower() == "exit":
+    #         exit()
+    # tlang = tlang[0].upper() + tlang[1:].lower()
 
+    # langa = ""
+    # while langa == "":
+    #     langa = input("Enter the first language: ")
+    #     if langa == "":
+    #         print("First language is required.")
+    #     if langa.lower() == "exit":
+    #         exit()
+    # langa = langa[0].upper() + langa[1:].lower()
+    
+    # langb = input("Enter the second language: ")
+    # if langb.lower() == "exit":
+    #     exit()
+    # if langb != "":
+    #     langb = langb[0].upper() + langb[1:].lower()
+    
+    tlang = "Spanish"
+    langa = "English"
+    langb = ""
 
-    # for i in range(1,501):
-    #     word = Word(i)
-    #     word.initialize(driver)
-    #     word.update()
-    #     if i % 20 == 0:
-    #         print(f"{i} words processed")
+    metadata = {
+        "words": normalized_words,
+        "length": len(normalized_words),
+        "name": name,
+        "filename": f'data/{name}.json',
+        "tlang": tlang,
+        "langa": langa,
+        "langb": langb,
+    }
+
+    # INITIALIZATION
+    for i in range(metadata["length"]):
+        word = Word(i+1, metadata["filename"])
+        word.initialize(metadata)
+        word.update()
+        if i % 20 == 0:
+            print(f"{i} words processed")
 
     # CREATION
+        
+
     # for i in range(483,484):
     #     word = Word(i)
 
@@ -248,6 +240,7 @@ Do you want to proceed? (y/n): """)
     #         print(f"{i} words processed")
 
     # CONVERSION
+
     # table = []
     # for i in range(1,501):
     #     word = Word(i)
@@ -255,48 +248,3 @@ Do you want to proceed? (y/n): """)
     #     table.append(row)
 
     # create_flashcards(table)
-
-    # STRESS MARKS
-#     driver = setup_driver()
-#     for i in range(1,501):
-
-#         word = Word(i)
-#         word.xtlang1 = stress_mark(word.xtlang1, driver)
-#         word.xtlang2 = stress_mark(word.xtlang2, driver)
-#         word.xtlang3 = stress_mark(word.xtlang3, driver)
-        
-#         if "|" in word.xtlang1:
-#             prompt = f"""Task: Correct the stress mark in the sentence {word.xtlang1} and paste the corrected sentence.
-# Provide only the requested answer
-
-# Example: 
-# Она́ сто́ит|стои́т на по́диуме и выступа́ет.
-# returns
-# Она́ стои́т на по́диуме и выступа́ет."""
-#             word.xtlang1 = ask(prompt)
-#         if "|" in word.xtlang2:
-#             prompt = f"""Task: Correct the stress mark in the sentence {word.xtlang2} and paste the corrected sentence.
-# Provide only the requested answer
-
-# Example: 
-# Она́ сто́ит|стои́т на по́диуме и выступа́ет.
-# returns
-# Она́ стои́т на по́диуме и выступа́ет."""
-#             word.xtlang2 = ask(prompt)
-#         if "|" in word.xtlang3:
-#             prompt = f"""Task: Correct the stress mark in the sentence {word.xtlang3} and paste the corrected sentence.
-# Provide only the requested answer
-
-# Example: 
-# Она́ сто́ит|стои́т на по́диуме и выступа́ет.
-# returns
-# Она́ стои́т на по́диуме и выступа́ет."""
-#             word.xtlang3 = ask(prompt)
-        
-#         word.update()
-#         print(word.xtlang1)
-
-#     close_driver(driver)
-
-
-    
