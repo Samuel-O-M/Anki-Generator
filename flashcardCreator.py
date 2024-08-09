2c4bb45eab0f5f7e05be487f101c2febdd477814
Samuel Orellana Mateo <samuel.orellanamateo@duke.edu>
2024-08-09 20:43:21 +0200

    Adapt flashcard structure

--- flashcardCreator.py
+++ flashcardCreator.py
@@ -1,11 +1,11 @@
 import csv
 
-def create_flashcards(data):
+def create_flashcards(header, content):
 
     with open('data/cards.csv', 'w', encoding='utf-8', newline='') as file:
         writer = csv.writer(file)
-        field = ["Top", "Russian", "Audio", "English", "Spanish", "Example Russian 1", "Example Russian 2", "Example Russian 3", "Audio 1", "Audio 2", "Audio 3", "Example English 1", "Example English 2", "Example English 3", "Example Spanish 1", "Example Spanish 2", "Example Spanish 3", "Tag"]
-        writer.writerow(field)
         
-        for array in data:
-                writer.writerow(array)
+        writer.writerow(header)
+        
+        for row in content:
+                writer.writerow(row)
